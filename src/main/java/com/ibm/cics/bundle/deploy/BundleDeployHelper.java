7b652606e0071dfe7264f3cd372f75a0fdd68c45
Alex Brown <brownal7@uk.ibm.com>
2026-01-30 10:33:05 +0000

    Add appConfigfile and addCICSAllAuth properties to WAR and EAR bundle parts.

--- src/main/java/com/ibm/cics/bundle/deploy/BundleDeployHelper.java
+++ src/main/java/com/ibm/cics/bundle/deploy/BundleDeployHelper.java
@@ -33,18 +33,18 @@ import org.apache.hc.client5.http.entity.mime.FileBody;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.entity.mime.StringBody;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
 import org.apache.hc.client5.http.ssl.TrustAllStrategy;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.Header;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -53,7 +53,9 @@ public class BundleDeployHelper {
 
 	private static final String AUTHORIZATION_HEADER = "Authorization";
 
-	public static void deployBundle(URI endpointURL, File bundle, String bunddef, String csdgroup, String cicsplex, String region, String username, char[] password, boolean allowSelfSignedCertificate) throws BundleDeployException, IOException {
+	public static void deployBundle(URI endpointURL, File bundle, String bunddef, String csdgroup, String cicsplex,
+			String region, String username, char[] password, boolean allowSelfSignedCertificate)
+			throws BundleDeployException, IOException {
 		MultipartEntityBuilder mpeb = MultipartEntityBuilder.create();
 		mpeb.addPart("bundle", new FileBody(bundle, ContentType.create("application/zip")));
 		mpeb.addPart("bunddef", new StringBody(bunddef, ContentType.TEXT_PLAIN));
@@ -64,63 +66,62 @@ public class BundleDeployHelper {
 		if (region != null && !region.isEmpty()) {
 			mpeb.addPart("region", new StringBody(region, ContentType.TEXT_PLAIN));
 		}
-		
-		
+
 		String path = endpointURL.getPath();
 		if (path == null) {
 			path = "";
 		} else if (!path.endsWith("/")) {
 			path = path + "/";
 		}
-		
+
 		path = path + "managedcicsbundles";
-		
+
 		URI target;
 		try {
 			target = new URI(
-				endpointURL.getScheme(),
-				endpointURL.getUserInfo(),
-				endpointURL.getHost(),
-				endpointURL.getPort(),
-				path,
-				endpointURL.getQuery(),
-				endpointURL.getFragment()
-			);
+					endpointURL.getScheme(),
+					endpointURL.getUserInfo(),
+					endpointURL.getHost(),
+					endpointURL.getPort(),
+					path,
+					endpointURL.getQuery(),
+					endpointURL.getFragment());
 		} catch (URISyntaxException e) {
 			throw new IOException(e);
 		}
-		
+
 		HttpPost httpPost = new HttpPost(target);
 		HttpEntity httpEntity = mpeb.build();
 		httpPost.setEntity(httpEntity);
-		
+
 		CloseableHttpClient httpClient;
 		if (!allowSelfSignedCertificate) {
 			httpClient = HttpClientBuilder.create().useSystemProperties().build();
 		} else {
 			try {
-                SSLContextBuilder sslContextBuilder = SSLContexts.custom().loadTrustMaterial(new TrustAllStrategy());
-                SSLConnectionSocketFactoryBuilder sslSocketFactoryBuilder = SSLConnectionSocketFactoryBuilder.create()
-                        .setSslContext(sslContextBuilder.build())
-                        .setHostnameVerifier(NoopHostnameVerifier.INSTANCE);
-                
-                PoolingHttpClientConnectionManagerBuilder connectionManagerBuilder = PoolingHttpClientConnectionManagerBuilder.create()
-                        .setSSLSocketFactory(sslSocketFactoryBuilder.build());
+				SSLContextBuilder sslContextBuilder = SSLContexts.custom().loadTrustMaterial(new TrustAllStrategy());
+				SSLConnectionSocketFactoryBuilder sslSocketFactoryBuilder = SSLConnectionSocketFactoryBuilder.create()
+						.setSslContext(sslContextBuilder.build())
+						.setHostnameVerifier(NoopHostnameVerifier.INSTANCE);
 
-                httpClient = HttpClients.custom()
-                        .setConnectionManager(connectionManagerBuilder.build())
-                        .useSystemProperties()
-                        .build();
+				PoolingHttpClientConnectionManagerBuilder connectionManagerBuilder = PoolingHttpClientConnectionManagerBuilder
+						.create()
+						.setSSLSocketFactory(sslSocketFactoryBuilder.build());
+
+				httpClient = HttpClients.custom()
+						.setConnectionManager(connectionManagerBuilder.build())
+						.useSystemProperties()
+						.build();
 			} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
 				throw new BundleDeployException("Error instantiating secure connection", e);
 			}
 		}
-		
+
 		String stringPassword = (password == null || password.length == 0) ? null : String.valueOf(password);
 		String credentials = username + ":" + stringPassword;
 		String encoding = Base64.getEncoder().encodeToString(credentials.getBytes("ISO-8859-1"));
 		httpPost.setHeader(AUTHORIZATION_HEADER, "Basic " + encoding);
-		
+
 		if (!bundle.exists()) {
 			throw new BundleDeployException("Bundle does not exist: '" + bundle + "'");
 		}
@@ -138,22 +139,24 @@ public class BundleDeployHelper {
 		}
 
 		if (responseStatusCode != 200) {
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
 			try {
 				String responseContent = bufferedReader.lines().collect(Collectors.joining());
 				if (contentType == null) {
 					throw new BundleDeployException("Http response: " + responseStatusCode);
 				} else if (contentType.equals("application/xml")) {
-					//liberty level error
+					// liberty level error
 					throw new BundleDeployException(responseContent);
 				} else if (contentType.equals("application/json")) {
-					//error from deploy endpoint
+					// error from deploy endpoint
 					ObjectMapper objectMapper = new ObjectMapper();
 					String responseMessage = objectMapper.readTree(responseContent).get("message").asText();
 					String responseErrors = "";
-					
+
 					if (responseMessage.contains("Some of the supplied parameters were invalid")) {
-						Iterator<Entry<String, JsonNode>> errorFields = objectMapper.readTree(responseContent).get("requestErrors").fields();
+						Iterator<Entry<String, JsonNode>> errorFields = objectMapper.readTree(responseContent)
+								.get("requestErrors").fields();
 						StringBuffer sb = new StringBuffer();
 						while (errorFields.hasNext()) {
 							Entry<String, JsonNode> errorField = errorFields.next();
@@ -164,11 +167,12 @@ public class BundleDeployHelper {
 						}
 						responseErrors = sb.toString();
 					} else if (responseMessage.contains("Bundle deployment failure")) {
-						responseErrors = objectMapper.readTree(responseContent).get("deployments").findValue("message").asText();
+						responseErrors = objectMapper.readTree(responseContent).get("deployments").findValue("message")
+								.asText();
 					}
 					throw new BundleDeployException(responseMessage + ":\n - " + responseErrors);
 				} else {
-					//CICS level error
+					// CICS level error
 					throw new BundleDeployException(responseContent);
 				}
 			} finally {
